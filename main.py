c95f0e073453c5cec49bafd9603407fb5fed71b6
Only1337 <47225461+Only1337@users.noreply.github.com>
2022-06-01 00:57:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import calendar
+import json
+import time
+import requests as r
+
+config = open('config.json')
+config = json.loads(config.read())
+number = config['number']
+msg = input("Enter your message here: ")
+
+
+def sendMessage(apiNumber,target,message):
+    d = datetime.utcnow()
+    getUnix = calendar.timegm(d.utctimetuple())
+    apiData = {"app": {"id": apiNumber,"time": getUnix,"data": {"recipient": {"id": target,},"message": [{"time": getUnix,"type": "text","value": message}]}}}
+    ok = r.post("https://whapi.io/api/send", json=apiData)
+    return "success sent +"+target # "["+ok.status_code+"] ["+getUnix+"] ["+ ok.content+"]"
+
+with open("victims.txt", "r") as a_file:
+  for line in a_file:
+    victim = line.strip()
+    print(sendMessage(number,victim,msg))
